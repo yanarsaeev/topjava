f1094ee92b5dd084ba98df7a5a2d2825245d2db6
yanarsaeev <mackardyback@internet.ru>
2025-02-18 22:20:49 +0300

    Add validation check, output of sort list

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -1,14 +1,16 @@
 package ru.javawebinar.topjava.repository.inmemory;
 
+import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.MealsUtil;
+import ru.javawebinar.topjava.util.ValidationUtil;
 
-import java.util.Collection;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
+@Repository
 public class InMemoryMealRepository implements MealRepository {
     private final Map<Integer, Meal> mealsMap = new ConcurrentHashMap<>();
     private final AtomicInteger counter = new AtomicInteger(0);
@@ -35,12 +37,14 @@ public class InMemoryMealRepository implements MealRepository {
 
     @Override
     public Meal get(int id) {
-        return mealsMap.get(id);
+        return ValidationUtil.checkNotFound(mealsMap.get(id), id);
     }
 
     @Override
     public Collection<Meal> getAll() {
-        return mealsMap.values();
+        List<Meal> mealList = new ArrayList<>(mealsMap.values());
+        mealList.sort(Comparator.comparing(Meal::getDateTime));
+        return mealList;
     }
 }
 
