71eb4e5e199b1c9956e32b909bdb5bdebe425e58
yanarsaeev <mackardyback@internet.ru>
2025-02-18 23:01:44 +0300

    Implement methods

--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -1,9 +1,34 @@
 package ru.javawebinar.topjava.service;
 
+import org.springframework.stereotype.Service;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
+import ru.javawebinar.topjava.util.ValidationUtil;
 
+import java.util.Collection;
+
+@Service
 public class MealService {
 
-    private MealRepository repository;
+    private final MealRepository repository;
 
+    public MealService(MealRepository repository) {
+        this.repository = repository;
+    }
+
+    public Meal create(Meal meal) {
+        return repository.save(meal);
+    }
+
+    public void delete(int id) {
+        ValidationUtil.checkNotFound(repository.get(id), id);
+    }
+
+    public void update(Meal meal) {
+        ValidationUtil.checkNotFound(repository.save(meal), meal.getId());
+    }
+
+    public Collection<Meal> getAll() {
+        return repository.getAll();
+    }
 }
