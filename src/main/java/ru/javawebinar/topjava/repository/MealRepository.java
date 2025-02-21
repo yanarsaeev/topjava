f76ff29c21481a857b6ae4b483cab76cdc23579d
yanarsaeev <mackardyback@internet.ru>
2025-02-21 23:07:17 +0300

    Add userId parameter

--- src/main/java/ru/javawebinar/topjava/repository/MealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/MealRepository.java
@@ -10,11 +10,11 @@ public interface MealRepository {
     Meal save(Meal meal);
 
     // false if meal does not belong to userId
-    boolean delete(int id);
+    boolean delete(int mealId, int userId);
 
     // null if meal does not belong to userId
-    Meal get(int id);
+    Meal get(int mealId, int userId);
 
     // ORDERED dateTime desc
-    Collection<Meal> getAll();
+    Collection<Meal> getAll(int userId);
 }
