dec83a2f62e0a00ea97aeac9010c38372d828b87
yanarsaeev <mackardyback@internet.ru>
2025-02-21 22:52:20 +0300

    Add logger, change methods

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -1,10 +1,11 @@
 package ru.javawebinar.topjava.repository.inmemory;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.MealsUtil;
-import ru.javawebinar.topjava.util.ValidationUtil;
 
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
@@ -12,7 +13,8 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 @Repository
 public class InMemoryMealRepository implements MealRepository {
-    private final Map<Integer, Meal> mealsMap = new ConcurrentHashMap<>();
+    private static final Logger log = LoggerFactory.getLogger(InMemoryMealRepository.class);
+    private final Map<Integer, List<Meal>> mealsMap = new ConcurrentHashMap<>();
     private final AtomicInteger counter = new AtomicInteger(0);
 
     {
@@ -21,30 +23,35 @@ public class InMemoryMealRepository implements MealRepository {
 
     @Override
     public Meal save(Meal meal) {
+        log.info("Save {}", meal);
         if (meal.isNew()) {
             meal.setId(counter.incrementAndGet());
-            mealsMap.put(meal.getId(), meal);
+            mealsMap.put(meal.getId(), new ArrayList<>(Collections.singletonList(meal)));
             return meal;
         }
-        // handle case: update, but not present in storage
-        return mealsMap.computeIfPresent(meal.getId(), (id, oldMeal) -> meal);
+
+        return meal;
     }
 
     @Override
-    public boolean delete(int id) {
-        return mealsMap.remove(id) != null;
+    public boolean delete(int id, int userId) {
+        log.info("Delete {} userId={}", id, userId);
+        List<Meal> meals = mealsMap.get(userId);
+        return meals.removeIf(meal -> meal.getId().equals(id));
     }
 
     @Override
-    public Meal get(int id) {
-        return ValidationUtil.checkNotFound(mealsMap.get(id), id);
+    public Meal get(int id, int userId) {
+        log.info("Get");
+        return mealsMap.get(userId).get(id);
     }
 
     @Override
-    public Collection<Meal> getAll() {
-        List<Meal> mealList = new ArrayList<>(mealsMap.values());
+    public Collection<Meal> getAll(int userId) {
+        log.info("getAll");
+        List<Meal> mealList = mealsMap.get(userId);
         mealList.sort(Comparator.comparing(Meal::getDateTime));
-        return mealList;
+        return null;
     }
 }
 
