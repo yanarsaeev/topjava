d8a298b166e2d3c3bf01c7780c2635799cd95439
yanarsaeev <mackardyback@internet.ru>
2025-02-21 22:55:05 +0300

    Add userId parameter

--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -16,19 +16,19 @@ public class MealService {
         this.repository = repository;
     }
 
-    public Meal create(Meal meal) {
+    public Meal create(Meal meal, int userId) {
         return repository.save(meal);
     }
 
-    public void delete(int id) {
-        ValidationUtil.checkNotFound(repository.get(id), id);
+    public void delete(int id, int userId) {
+        ValidationUtil.checkNotFound(repository.get(id, userId), id);
     }
 
-    public void update(Meal meal) {
+    public void update(Meal meal, int userId) {
         ValidationUtil.checkNotFound(repository.save(meal), meal.getId());
     }
 
-    public Collection<Meal> getAll() {
-        return repository.getAll();
+    public Collection<Meal> getAll(int userId) {
+        return repository.getAll(userId);
     }
 }
